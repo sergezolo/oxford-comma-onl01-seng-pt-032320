4baa535640de47f53890289a072b19ffe40c1974
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-03-30 18:46:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.lenght == 2
+    return array.join(" and ")
+  elsif array.lenght >= 3
+    new_last = "and #{array[-1]}" 
+    array.pop
+    array.push(new_last)
+    return array.join(", ")
 end
